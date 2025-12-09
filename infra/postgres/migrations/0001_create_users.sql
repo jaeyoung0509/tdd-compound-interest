95fadaa9cd1c971ada8febad0df030906ee47e16
jaeyoung0509 <jaeyoung0509@naver.com>
2025-12-09 10:09:05 +0000

    refactor:  `updated_at` column to `users` table.

--- infra/postgres/migrations/0001_create_users.sql
+++ infra/postgres/migrations/0001_create_users.sql
@@ -2,6 +2,7 @@ CREATE TABLE users (
     id         CHAR(26)     PRIMARY KEY,
     name       VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ  NOT NULL DEFAULT NOW()
+    updated_at TIMESTAMPTZ  NOT NULL DEFAULT NOW()
 );
 
 COMMENT ON TABLE users IS 'User aggregate storing identity info';
